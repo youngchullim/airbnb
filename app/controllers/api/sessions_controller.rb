33fbed15b3901edf2463f0fbf15340c5b1e747ac
Young Chul Lim <youngchullim95@gmail.com>
2019-08-03 14:53:57 -0700

    Add SessionsController skeleton

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by(id: params[:id])
+    if @user
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: ["Incorrect email or password"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout(@user)
+      render json: {}
+    else
+      render json: ["No user logged in"], status: 404
+    end
+  end
+end
