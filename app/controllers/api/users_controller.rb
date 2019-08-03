e2284a5c141625c9f0e012633a5a662162e62783
Young Chul Lim <youngchullim95@gmail.com>
2019-08-03 12:34:27 -0700

    Add UsersController skeleton

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: ["Invalid input. Please try again."], status: 401
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password)
+  end
+end
