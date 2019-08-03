ac54a7e7ffa964c0d974465deff7fe5915701ebf
Young Chul Lim <youngchullim95@gmail.com>
2019-08-03 12:52:19 -0700

    Add basic UserModel skeleton

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,41 @@
 #
 
 class User < ApplicationRecord
+  attr_reader :password
+
+  validates :email, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :first_name, :last_name, presence: true
+
+  before_validation :ensure_session_token
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
 end
