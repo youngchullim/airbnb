45d43d6f931a8140b8b000aa82fc0e284a812523
Young Chul Lim <youngchullim95@gmail.com>
2019-08-03 14:55:07 -0700

    Add routes to Session Create and Destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:show, :create]
+    resources :sessions, only: [:create, :destroy]
   end
 end
