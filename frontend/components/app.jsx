972d721680f33aba973fe7e646bb71178953a694
Young Chul Lim <youngchullim95@gmail.com>
2019-08-03 21:38:06 -0700

    Add App Component that Switches from login signup and splashPage

---
+++ frontend/components/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute }from '../util/route_util';
+
+import GreetingContainer from './greeting/greeting_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
+// import Modal from './modal/modal';
+
+
+const App = () => {
+  return (
+    <div className="app-comp">
+      {/* <Modal /> */}
+      
+      <Switch>
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <Route path="/" component={GreetingContainer}/>
+      </Switch>
+    </div>
+  )
+};
+
+export default App;
