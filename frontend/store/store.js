adcc701354a68050135a1d10b58ef99c014f8080
Young Chul Lim <youngchullim95@gmail.com>
2019-08-03 21:40:23 -0700

    Add configureStore skeleton with both withLogger and withoutLogger

---
+++ frontend/store/store.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/root_reducer';
+
+// @@@@@@   WITHOUT LOGGER @@@@@@
+// const configureStore = (preloadedState = {}) => {
+//   return createStore(
+//     rootReducer, 
+//     preloadedState, 
+//     applyMiddleware(thunk)
+//   )
+// };
+
+// @@@@@@@   WITH LOGGER   @@@@@@@
+const configureStore = (preloadedState = {}) => {
+  return createStore(
+    rootReducer, 
+    preloadedState, 
+    applyMiddleware(thunk, logger)
+  )
+};
+
+export default configureStore;
