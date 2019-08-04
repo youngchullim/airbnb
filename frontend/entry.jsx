58f21d814c27c52a38bbd67c3640c6163e0eb7fd
Young Chul Lim <youngchullim95@gmail.com>
2019-08-03 21:35:02 -0700

    Add basic state skeleton in Entry

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // ReactDOM.render(<h1>Welcome to AirBnB</h1>, root);
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { 
+        id: window.currentUser.id 
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to AirBnB</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root)
+
 });
