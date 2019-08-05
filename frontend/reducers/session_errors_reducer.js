ad6e184a5fc976fa0448489e3ba528465e5edaf1
Young Chul Lim <youngchullim95@gmail.com>
2019-08-05 11:05:53 -0700

    Add sessionErrorsReducer skeleton

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,22 @@
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_SESSION_ERRORS,
+  CLEAR_ERRORS
+} from '../actions/session_actions';
+
+const sessionErrorsReducer = (oldState=[], action) => {
+  Object.freeze(oldState);
+
+  switch(action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+      return [];
+    case CLEAR_ERRORS:
+      return [];
+    default:
+      return oldState;
+  }
+};
+
+export default sessionErrorsReducer;
