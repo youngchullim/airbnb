1273abe653caba9eb9db360ae7983a6c1956bc1a
Young Chul Lim <youngchullim95@gmail.com>
2019-08-12 14:48:13 -0700

    Add greeting Component with backgroundImg and greetingSearchForm skeleton

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -93,69 +93,79 @@ class Greeting extends React.Component {
               </li>
             </ul>
           </div>
-          <div className="greeting-search">
-{/* SEARCH METHOD */}
-            <form onSubmit={this.handleSearch} className="greeting-search-form">
-              {/* <div className="login-error-message">{this.renderErrors()}</div> */}
-              <div className="greeting-search-info">
-                <label>
-                  <input type="text"
-                    className="location-input"
-                    value={this.state.location}
-                    onChange={this.update('location')}
-                    placeholder="Location"
-                    />
-                </label>
-                <div className="greeting-date">
-                  Date: 
-                  <p className="greeting-date-range-picker">
-                    <DateRangePicker
-                      startDate={this.state.startDate} 
-                      startDateId="your_unique_start_date_id"
-                      endDate={this.state.endDate}
-                      endDateId="your_unique_end_date_id"
-                      onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} 
-                      focusedInput={this.state.focusedInput}
-                      onFocusChange={focusedInput => this.setState({ focusedInput })}
-                      startDatePlaceholderText="Check in"
-                      endDatePlaceholderText='Check out'
-                      numberOfMonths={1}
-                    />
-                  </p>
-                </div>
-                  {/* startDate turns into
-                  {console.log(new Date(this.state.startDate))}
-                  Tue Aug 20 2019 12:00:00 GMT-0700 (Pacific Daylight Time) */}
-                <div className="greeting-adults-wrapper">
-                  <select className="greeting-adults" onChange={this.update('adults')}>
-                    <option value="1">1 adult</option>
-                    <option value="2">2 adults</option>
-                    <option value="3">3 adults</option>
-                    <option value="4">4 adults</option>
-                    <option value="5">5 adults</option>
-                    <option value="6">6 adults</option>
-                    <option value="7">7 adults</option>
-                    <option value="8">8 adults</option>
-                    <option value="9">9 adults</option>
-                    <option value="10">10 adults</option>
-                    <option value="11">11 adults</option>
-                    <option value="12">12 adults</option>
-                  </select> 
-                </div>
-                <div className="greeting-children-wrapper">
-                  <select className="greeting-children" onChange={this.update('children')}>
-                    <option value="0">0 children</option>
-                    <option value="1">1 child</option>
-                    <option value="2">2 children</option>
-                    <option value="3">3 children</option>
-                    <option value="4">4 children</option>
-                    <option value="5">5 children</option>
-                  </select> 
-                </div>
-
-                <input className="greeting-search-button" type="submit" value={"Search"} />
+          <div className="greeting-search-wrapper">
+            <div className="greeting-search">
+              <div className="greeting-search-title">
+                <h1 className="search-h1">Find places to stay in United States on Airbnb</h1>
+                <p className="search-p">Discover entire homes and private rooms perfect for any trip.</p>
               </div>
-            </form>
+  {/* SEARCH METHOD */}
+              <form onSubmit={this.handleSearch} className="greeting-search-form">
+                {/* <div className="login-error-message">{this.renderErrors()}</div> */}
+                <div className="greeting-search-info">
+                  <label>
+                    <p className="greeting-search-tag">WHERE</p>
+                    <input type="text"
+                      className="location-input"
+                      value={this.state.location}
+                      onChange={this.update('location')}
+                      placeholder="Location"
+                      />
+                  </label>
+                  <div className="greeting-date">
+                    <p className="greeting-search-tag">WHEN</p>
+                    <p className="greeting-date-range-picker">
+                      <DateRangePicker
+                        startDate={this.state.startDate} 
+                        startDateId="your_unique_start_date_id"
+                        endDate={this.state.endDate}
+                        endDateId="your_unique_end_date_id"
+                        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} 
+                        focusedInput={this.state.focusedInput}
+                        onFocusChange={focusedInput => this.setState({ focusedInput })}
+                        startDatePlaceholderText="Check in"
+                        endDatePlaceholderText='Check out'
+                        numberOfMonths={1}
+                      />
+                    </p>
+                  </div>
+                    {/* startDate turns into
+                    {console.log(new Date(this.state.startDate))}
+                    Tue Aug 20 2019 12:00:00 GMT-0700 (Pacific Daylight Time) */}
+                  <div className="greeting-guests">
+                    <p className="greeting-search-tag">ADULTS</p>
+                    <div className="greeting-adults-wrapper">
+                      <select className="greeting-adults" onChange={this.update('adults')}>
+                        <option value="1">1 adult</option>
+                        <option value="2">2 adults</option>
+                        <option value="3">3 adults</option>
+                        <option value="4">4 adults</option>
+                        <option value="5">5 adults</option>
+                        <option value="6">6 adults</option>
+                        <option value="7">7 adults</option>
+                        <option value="8">8 adults</option>
+                        <option value="9">9 adults</option>
+                        <option value="10">10 adults</option>
+                        <option value="11">11 adults</option>
+                        <option value="12">12 adults</option>
+                      </select> 
+                    </div>
+                    <div className="greeting-children-wrapper">
+                      <p className="greeting-search-tag">CHILDREN</p>
+                      <select className="greeting-children" onChange={this.update('children')}>
+                        <option value="0">0 children</option>
+                        <option value="1">1 child</option>
+                        <option value="2">2 children</option>
+                        <option value="3">3 children</option>
+                        <option value="4">4 children</option>
+                        <option value="5">5 children</option>
+                      </select> 
+                    </div>
+                  </div>
+                  <input className="greeting-search-button" type="submit" value={"Search"} />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       );
